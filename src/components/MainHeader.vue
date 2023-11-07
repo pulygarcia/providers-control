319b3ed390e74684c860cfaa86ec2fc0137d6562
puly garcia <pulygarcia09@gmail.com>
2023-11-07 16:25:31 -0300

    hiding or showing user info and actions when there is current user, logout and login finished

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,8 @@
 <script setup>
   import { RouterLink } from 'vue-router';
+  import {useAuthStore} from '../stores/authStore'
+
+  const authStore = useAuthStore();
 </script>
 
 <template>
@@ -15,14 +18,23 @@
 
           Admin panel
         </RouterLink>
-        <RouterLink :to="{name: 'login'}" class="flex items-center gap-2 rounded py-1 px-2 hover:shadow-lg transition bg-blue-950">
+        <RouterLink v-if="!authStore.currentUser" :to="{name: 'login'}" class="flex items-center gap-2 rounded py-1 px-2 hover:shadow-lg transition bg-blue-950">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
           </svg>
 
           Login
         </RouterLink>
+
+        <button v-else @click="authStore.logout" class="flex items-center gap-2 rounded py-1 px-2 hover:shadow-lg transition bg-red-500 text-white">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+          </svg>
+
+          Close session
+        </button>
       </nav>
     </div>
+    <p v-if="authStore.currentUser" class="container mx-auto mt-2 font-bold">User: <span class="font-normal text-gray-300">{{ authStore.userName }}</span></p>
   </header>
 </template>
--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,12 +1,17 @@
 import {defineStore} from 'pinia'
 import { ref, onMounted } from 'vue';
+import {useRouter} from 'vue-router'
 import {useFirebaseAuth} from 'vuefire'
-import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut  } from "firebase/auth";
 
 export const useAuthStore = defineStore('auth', () => {
+    const router = useRouter();
+
     const auth = useFirebaseAuth();
     const currentError = ref('');
     const currentUser = ref(null);
+    const userName = ref(null);
+    const successMessage = ref('');
 
     const errors = {
         'auth/invalid-login-credentials' : 'Invalid login credentials',
@@ -17,8 +22,10 @@ export const useAuthStore = defineStore('auth', () => {
         onAuthStateChanged(auth, (user) => {
             if(user){
                 currentUser.value = user;
+                userName.value = currentUser.value.email;
             }
         })
+
     })
     
     const login = (values) => {
@@ -27,16 +34,33 @@ export const useAuthStore = defineStore('auth', () => {
             // Signed in 
             currentUser.value = userCredential.user;
             //console.log(currentUser.value);
+            router.push({name: 'admin-providers'})
           })
           .catch((error) => {
             currentError.value = errors[error.code];
-            //console.log(currentError.value);
+        });
+    }
+
+    const logout = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            currentUser.value = null;
+            successMessage.value = 'User session was closed';
+            setTimeout(() => {
+                successMessage.value = '';
+                router.push({name: 'login'})
+            }, 3000);
+
+        }).catch((error) => {
+            console.log(error);
         });
     }
     
     return{
         login,
         currentUser,
-        currentError
+        currentError,
+        userName,
+        logout
     }
 })
